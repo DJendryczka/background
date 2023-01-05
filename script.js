ec51288d061304595e3637b496eccca613d77627
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2023-01-05 19:39:43 +0100

    Add functionality working ok

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const css = document.querySelector('h3');
 const colorOne = document.querySelector('.color1');
 const colorTwo = document.querySelector('.color2');
+const body = document.querySelector('#gradient');
+console.log(body)
+
+const setGradient = ()=>{
+    body.style.background = 'linear-gradient(to right, ' 
+    + colorOne.value
+    + ','
+    + colorTwo.value
+    + ')';
+    css.textContent = body.style.background + ';'
+}
+
+colorOne.addEventListener('input', setGradient)
+
+colorTwo.addEventListener('input', setGradient)
